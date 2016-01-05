6570329817c213c827e44d049246bafd4e5ced95
Dimitri DUPART <ddupart@e2r7p7.42.fr>
2016-01-05 19:30:04 +0100

    okay

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddupart <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/05 17:09:32 by ddupart           #+#    #+#             */
+/*   Updated: 2016/01/05 19:28:28 by ddupart          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 static size_t		ft_end(char *stock)
@@ -5,14 +17,14 @@ static size_t		ft_end(char *stock)
 	int			i;
 
 	i = 0;
-	while (stock[i] != '\n')
+	while (stock[i] != '\n' && stock[i] != '\0')
 		i++;
 	return (i);
 }
 
-static char		*ft_gnl_join(char *s1, char *s2)
+static char			*ft_gnl_join(char *s1, char *s2)
 {
-	char		*dst;
+	char			*dst;
 	size_t			a;
 	size_t			b;
 
@@ -28,23 +40,41 @@ static char		*ft_gnl_join(char *s1, char *s2)
 	if (b)
 		ft_memcpy(dst + a, s2, b);
 	free(s1);
-	ft_bzero(s2, (size_t)BUFF_SIZE + 1);
+	ft_bzero(s2, BUFF_SIZE + 1);
 	return (dst);
 }
 
-static int		ft_end_line(char *buf, char **line, char **stock)
+static int			ft_end_line(char *buf, char **line, char **stock)
 {
-	*stock = ft_gnl_join(*stock, buf);
+	size_t		end;
+
+	if (!(*stock = ft_gnl_join(*stock, buf)))
+		return (-1);
+	end = ft_end(*stock);
 	if (ft_strchr(*stock, '\n') != NULL)
 	{
-		*line = ft_strsub(*stock, 0, ft_end(*stock));
-		*stock = ft_strsub(*stock, ft_end(*stock) + 1, ft_strlen(*stock) - ft_end(*stock) + 1);
+		if (!(*line = ft_strsub(*stock, 0, end)))
+			return (-1);
+		if (!(*stock = ft_strsub(*stock, end + 1, ft_strlen(*stock) - end + 1)))
+			return (-1);
 		return (1);
 	}
 	return (0);
 }
 
-int		get_next_line(int const fd, char **line)
+int					ft_no_nl(char **line, char **stock)
+{
+	size_t		end;
+
+	end = ft_end(*stock);
+	if (!(*line = ft_strsub(*stock, 0, end)))
+		return (-1);
+	if (!(*stock = ft_strsub(*stock, end + 1, ft_strlen(*stock) - end + 1)))
+		return (-1);
+	return (0);
+}
+
+int					get_next_line(int const fd, char **line)
 {
 	static char	*stock = NULL;
 	int			ret;
@@ -63,33 +93,10 @@ int		get_next_line(int const fd, char **line)
 	}
 	if ((verif = ft_end_line(buf, line, &stock)) == 0)
 	{
+		if (ft_strlen(stock) > 0)
+			verif = ft_no_nl(line, &stock);
 		free(buf);
 		ft_strdel(&stock);
 	}
 	return (verif);
 }
-/*
-int	main(int argc, char **argv)
-{
-	int	fd;
-	char *line;
-	int	control;
-	int	count;
-
-	count = 0;
-	fd = open(argv[1], O_RDONLY);
-	control = 1;
-	while (get_next_line(fd, &line) > 0)
-	{
-		if (line != NULL)
-		{
-		ft_putstr("Line ");
-		ft_putnbr(count);
-		ft_putstr(" : ");
-		ft_putendl(line);
-		count++;
-		}
-	}
-	free(line);
-	return (0);
-}*/
